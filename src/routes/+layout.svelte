ed0ff36294eb9fb2bd9bee92c62ac5cacd74f583
BISHER-AL-MASRI <bbgyttv@gmail.com>
2025-06-04 11:50:46 -0400

    e

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 	import '../app.css';
 
 	let { children } = $props();
